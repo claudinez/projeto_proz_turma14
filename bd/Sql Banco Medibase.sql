6ce74de749eb24c00a95357f424c5c7f424e2bea
claudinez <andrade.claudinez@gmail.com>
2023-11-23 09:45:21 -0300

    Correção nas chaves estrangeiras do movimento dos paciente

--- bd/Sql Banco Medibase.sql
+++ bd/Sql Banco Medibase.sql
@@ -13,21 +13,21 @@ CREATE TABLE "medico" (
   "med_fone" "CHAR (20)" NOT NULL,
   "med_crm" INT UNIQUE NOT NULL,
   "med_uf_crm" VARCHAR(2) NOT NULL,
-  "med_ativo" "char (1)"
+  "med_ativo" "CHAR (1)"
 );
 
 CREATE TABLE "especialidade" (
   "esp_id" SERIAL PRIMARY KEY,
   "esp_nome_especialidade" VARCHAR(100) NOT NULL,
   "esp_codigo_especialidade" SMALLINT UNIQUE NOT NULL,
-  "esp_ativo" "char (1)"
+  "esp_ativo" "CHAR (1)"
 );
 
 CREATE TABLE "medico_especialidade" (
   "med_esp_id" SERIAL PRIMARY KEY,
   "fk_med_id" INT NOT NULL,
   "fk_esp_id" INT NOT NULL,
-  "med_esp_ativo" "char (1)" NOT NULL
+  "med_esp_ativo" "CHAR (1)" NOT NULL
 );
 
 CREATE TABLE "convenio" (
@@ -35,7 +35,7 @@ CREATE TABLE "convenio" (
   "con_nome_convenio" VARCHAR(50) NOT NULL,
   "con_tipo_plano" "CHAR (3)" NOT NULL,
   "con_fone" "CHAR (20)" NOT NULL,
-  "con_ativo" "char (1)"
+  "con_ativo" "CHAR (1)"
 );
 
 CREATE TABLE "paciente" (
@@ -55,7 +55,7 @@ CREATE TABLE "paciente" (
   "pac_nome_pai" VARCHAR(100),
   "pac_responsavel" VARCHAR(100) NOT NULL,
   "fk_con_id" INT NOT NULL,
-  "pac_ativo" "char (1)"
+  "pac_ativo" "CHAR (1)"
 );
 
 CREATE TABLE "mov_consulta_paciente" (
@@ -66,20 +66,20 @@ CREATE TABLE "mov_consulta_paciente" (
   "fk_con_id" INT NOT NULL,
   "mov_data_atende" DATE NOT NULL,
   "mov_hora_atende" TIME NOT NULL,
-  "mov_ativo" "char (1)",
+  "mov_ativo" "CHAR (1)",
   "mov_evolucao_medica" TEXT
 );
 
-ALTER TABLE "medico_especialidade" ADD CONSTRAINT "fk_med_id" FOREIGN KEY ("fk_med_id") REFERENCES "medico" ("med_id");
+ALTER TABLE "medico_especialidade" ADD CONSTRAINT "fk_medico_especialidade_medico" FOREIGN KEY ("fk_med_id") REFERENCES "medico" ("med_id");
 
-ALTER TABLE "medico_especialidade" ADD CONSTRAINT "fk_esp_id" FOREIGN KEY ("fk_esp_id") REFERENCES "especialidade" ("esp_id");
+ALTER TABLE "medico_especialidade" ADD CONSTRAINT "fk_medico_especialidade_especialidade" FOREIGN KEY ("fk_esp_id") REFERENCES "especialidade" ("esp_id");
 
-ALTER TABLE "paciente" ADD CONSTRAINT "fk_con_id" FOREIGN KEY ("fk_con_id") REFERENCES "convenio" ("con_id");
+ALTER TABLE "paciente" ADD CONSTRAINT "fk_paciente_convenio" FOREIGN KEY ("fk_con_id") REFERENCES "convenio" ("con_id");
 
-ALTER TABLE "mov_consulta_paciente" ADD FOREIGN KEY ("fk_pac_prontuario") REFERENCES "paciente" ("pac_prontuario");
+ALTER TABLE "mov_consulta_paciente" ADD CONSTRAINT "fk_mov_consulta_paciente_paciente" FOREIGN KEY ("fk_pac_prontuario") REFERENCES "paciente" ("pac_prontuario");
 
-ALTER TABLE "mov_consulta_paciente" ADD FOREIGN KEY ("fk_med_id") REFERENCES "medico" ("med_id");
+ALTER TABLE "mov_consulta_paciente" ADD CONSTRAINT "fk_mov_consulta_paciente_medico" FOREIGN KEY ("fk_med_id") REFERENCES "medico" ("med_id");
 
-ALTER TABLE "mov_consulta_paciente" ADD FOREIGN KEY ("fk_esp_id") REFERENCES "especialidade" ("esp_id");
+ALTER TABLE "mov_consulta_paciente" ADD CONSTRAINT "fk_mov_consulta_paciente_especialidade" FOREIGN KEY ("fk_esp_id") REFERENCES "especialidade" ("esp_id");
 
-ALTER TABLE "mov_consulta_paciente" ADD FOREIGN KEY ("fk_con_id") REFERENCES "convenio" ("con_id");
+ALTER TABLE "mov_consulta_paciente" ADD CONSTRAINT "fk_mov_consulta_paciente_convenio" FOREIGN KEY ("fk_con_id") REFERENCES "convenio" ("con_id");
